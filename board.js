5a680b4971123493555120c6408404ca87bbfa24
Charles Jaimet <cjaimet@pipsc.ca>
2020-04-19 14:51:45 -0400

    Init

---
+++ board.js
@@ -0,0 +1,124 @@
+var bgl_timeout  = 180;
+var bgl_timer    = null;
+var elem_clock   = null;
+var elem_button  = null;
+
+function setBoard() {
+  elem_button = document.getElementById( 'timerButton' );
+  elem_clock  = document.getElementById( 'timerDisplay' );
+  setTimerDisplay();
+  var dice = setDice();
+  var dice_count = dice.length;
+  var die_number = 0;
+  var die_char  = '';
+  var die_style  = '';
+  for ( x = 0; x < dice_count; x++ ) {
+    die_number = getDieNumber( dice );
+    die_char   = getDieFace( dice, die_number );
+    setDieFace( x, die_char, die_style );
+    setDieStyle( x, die_char );
+    dice.splice( die_number, 1 );
+  }
+}
+
+function setDieStyle( cell_number, die_char ) {
+  var rotate = Math.floor( Math.random() * 4 ) * 90;
+  var style = '';
+  var cell = document.getElementById( 'bgl_cell_' + cell_number );
+  if ( 0 !== rotate ) {
+    cell.style.transform = 'rotate( ' + rotate + 'deg )';
+  }
+  if ( 'M' === die_char || 'W' === die_char ) {
+    cell.style.textDecoration = 'underline';
+  }
+}
+
+function setDieFace( cell_number, die_char, die_style ) {
+  document.getElementById( 'bgl_cell_' + cell_number ).innerText = die_char;
+}
+
+function getDieFace( dice, die_number ) {
+  die_face = Math.floor( Math.random() * 6 );
+  return dice[ die_number ][ die_face ];
+}
+
+function getDieNumber( dice ) {
+  return Math.floor( Math.random() * dice.length );
+}
+
+function setDice() {
+  return getDice();
+}
+
+function getDice() {
+  return [
+    [ 'A', 'O', 'J', 'O', 'B', 'B' ],
+    [ 'E', 'E', 'A', 'A', 'G', 'N' ],
+    [ 'H', 'W', 'T', 'R', 'E', 'V' ],
+    [ 'S', 'N', 'U', 'I', 'E', 'E' ],
+    [ 'H', 'R', 'N', 'N', 'L', 'Z' ],
+    [ 'W', 'E', 'N', 'E', 'G', 'H' ],
+    [ 'W', 'T', 'A', 'O', 'O', 'T' ],
+    [ 'M', 'T', 'C', 'U', 'I', 'O' ],
+    [ 'I', 'O', 'T', 'S', 'S', 'E' ],
+    [ 'A', 'S', 'K', 'P', 'F', 'F' ],
+    [ 'S', 'T', 'Y', 'T', 'I', 'D' ],
+    [ 'I', 'D', 'E', 'R', 'X', 'L' ],
+    [ 'L', 'R', 'T', 'E', 'T', 'Y' ],
+    [ 'C', 'H', 'A', 'P', 'O', 'S' ],
+    [ 'S', 'S', 'E', 'T', 'I', 'O' ],
+    [ 'Qu', 'U', 'N', 'H', 'I', 'M' ],
+  ];
+}
+
+function displayDice( mode ) {
+  var elem = document.getElementsByClassName( 'die_cube' );
+  var css  = 'die_shown';
+  for ( var i = 0; i < elem.length; i++ ) {
+    if ( 'Hide' !== mode ) {
+      elem[ i ].classList.add( css );
+    } else {
+      elem[ i ].classList.remove( css );
+    }
+  }
+}
+
+function setTimer() {
+  var mode = elem_button.value;
+  if ( 0 >= bgl_timeout ) {
+    displayDice( 'Show' );
+  } else {
+    var next = 'Pause';
+    if ( 'Pause' === mode ) {
+      next = 'Start'
+      clearInterval( bgl_timer );
+      displayDice( 'Hide' );
+    } else {
+      bgl_timer = setInterval( activateTimer, 1000 );
+      displayDice( 'Show' );
+    }
+    setButton( next, next );
+  }
+}
+
+function activateTimer() {
+  bgl_timeout --;
+  setTimerDisplay();
+  if ( 0 === bgl_timeout ) {
+    clearInterval( bgl_timer );
+    displayDice( 'Hide' );
+    setButton( 'Show', 'Score' );
+  }
+}
+
+function setTimerDisplay() {
+  var minutes = Math.floor( bgl_timeout / 60 );
+  var seconds = bgl_timeout - ( 60 * minutes );
+  var clock_display = minutes + ':' + ( 10 > seconds ? '0' : '' ) + seconds;
+  elem_clock.innerText = clock_display;
+}
+
+function setButton( val, lbl ) {
+  elem_button.value     = val;
+  elem_button.innerText = lbl;
+}
